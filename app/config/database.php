b516029a5a8b477e3b5452be9d343631d0cd4829
Artem Shklyarik <artem-shklyarik@yandex.ru>
2018-03-22 19:54:50 +0300

    install composer and add structure folders to the project

---
+++ app/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host'     => 'localhost',
+    'database' => 'rocketcmf',
+    'user'     => 'root',
+    'password' => '',
+    'port'     => '3306'
+];
---
+++ app/modules/Rocket/Demo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'enable'  => true,
+    'version' => '0.1'
+];
