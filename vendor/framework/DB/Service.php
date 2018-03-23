955cebcbf35186629b70a74544cf1c86a22903b2
Artem Shklyarik <artem-shklyarik@yandex.ru>
2018-03-23 19:22:58 +0300

    unistall doctrine 3

--- vendor/framework/DB/Service.php
+++ vendor/framework/DB/Service.php
@@ -3,28 +3,12 @@
 namespace Framework\DB;
 
 use Framework\DI;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
 
 final class Service extends \Framework\Service
 {
     public function start(DI $di)
     {
         $databaseConfig = $di->get('config')->getDatabaseConfig();
-
-        $isDevMode = true;
-
-        $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/var/lib/Doctrine/"), $isDevMode);
-
-        $dbParams = array(
-            'driver'   => 'pdo_mysql',
-            'user'     => $databaseConfig['mysql']['user'],
-            'password' => $databaseConfig['mysql']['password'],
-            'dbname'   => $databaseConfig['mysql']['database'],
-        );
-
-        $entityManager = EntityManager::create($dbParams, $config);
-        var_dump($entityManager);
     }
 
     public function finish(DI $di)
