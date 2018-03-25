6422323d2416c8b84f98130e5c730cb11706c6b5
Artem Shklyarik <artem-shklyarik@yandex.ru>
2018-03-25 16:11:46 +0300

    add website manager and language manager to services list

---
+++ vendor/framework/LanguageManager/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Framework\LanguageManager;
+
+use Framework\DI;
+
+final class Service extends \Framework\Service
+{
+    public function start(DI $di)
+    {
+        //  TODO:: language manager service
+    }
+
+    public function finish(DI $di)
+    {
+        return;
+    }
+}
--- vendor/framework/services.php
+++ vendor/framework/services.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    'session'        => 'Framework\Session\Service',
-    'file_manager'   => 'Framework\FileManager\Service',
-    'config'         => 'Framework\Config\Service',
-    'http'           => 'Framework\Http\Service',
-    'db'             => 'Framework\DB\Service',
-    'module_manager' => 'Framework\ModuleManager\Service',
-    'event_manager'  => 'Framework\EventManager\Service',
-    'router'         => 'Framework\Router\Service',
+    'session'          => 'Framework\Session\Service',
+    'file_manager'     => 'Framework\FileManager\Service',
+    'config'           => 'Framework\Config\Service',
+    'http'             => 'Framework\Http\Service',
+    'db'               => 'Framework\DB\Service',
+    'module_manager'   => 'Framework\ModuleManager\Service',
+    'website_manager'  => 'Framework\WebsiteManager\Service',
+    'language_manager' => 'Framework\LanguageManager\Service',
+    'event_manager'    => 'Framework\EventManager\Service',
+    'router'           => 'Framework\Router\Service',
 ];
